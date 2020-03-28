f39941e2edf547e3a557a32c3624a39851d85240
JonathanMichaelEdwards <jon.michael.edwards13@gmail.com>
2020-03-28 16:19:50 +1300

    ball collision - updates

--- Projects/Assignment 1/Museum/src/display.cpp
+++ Projects/Assignment 1/Museum/src/display.cpp
@@ -398,7 +398,7 @@ void ball(void)
 // void bounceUpAndDown(int value) 
 // {  
 	// if (_spacePressed) {  
-	// 	if (ballPosY >= FLOOR_BED+BALL_RADIUS)
+	// 	if (ballPosY+speedY >= FLOOR_BED+BALL_RADIUS)
 	// 		speedY -= sin(_velTheta) * t * sin(_velTheta) - 0.5 * GRAVITY * (t*t);   // acceleration (drag)
 	// 	else 
 	// 		speedY *= -1;                                                            // change direction
@@ -415,67 +415,6 @@ void ball(void)
 
 bool reset = false;
 
-
-
-float cx = 0;      // circle position (set with mouse)
-float cy = 0;
-float r = 30;      // circle radius
-
-// float sx = 200;    // square position
-// float sy = 100;
-// float sw = 200;    // and dimensions
-// float sh = 200;
-
-
-
-// void draw() {
-//   background(255);
-
-//   // update square to mouse coordinates
-//   cx = mouseX;
-//   cy = mouseY;
-
-//   // check for collision
-//   // if hit, change rectangle color
-//   bool hit = circleRect(cx,cy,r, sx,sy,sw,sh);
-//   if (hit) {
-//     fill(255,150,0);
-//   }
-//   else {
-//     fill(0,150,255);
-//   }
-//   rect(sx,sy, sw,sh);
-
-//   // draw the circle
-//   fill(0, 150);
-//   ellipse(cx,cy, r*2,r*2);
-// }
-
-
-// CIRCLE/RECTANGLE
-bool circleRect(float cx, float cy, float radius) {
-
-  // temporary variables to set edges for testing
-  float testX = cx;
-  float testY = cy;
-
-  // which edge is closest?
-//   if (cx < rx)         testX = rx;      // test left edge
-//   else if (cx > rx+rw) testX = rx+rw;   // right edge
-//   if (cy < ry)         testY = ry;      // top edge
-  if (cy > -1) testY = -1;   // bottom edge
-
-  // get distance from closest edges
-  float distX = cx-testX;
-  float distY = cy-testY;
-  float distance = sqrt( (distX*distX) + (distY*distY) );
-
-  // if the distance is less than the radius, collision!
-  if (distance <= radius) {
-    return true;
-  }
-  return false;
-}
 
 
 void rstBall(void) 
@@ -502,13 +441,12 @@ void ballBounce(int value)
 		if (ballPosY+speedY >= FLOOR_BED+BALL_RADIUS) {
 			reset = false;
 			speedY -=  sin(_velTheta) * t * sin(_velTheta) - 0.5 * GRAVITY * (t*t);   // Gravity acceleration movement (drag)
-			ballPosY += speedY;
 		} else {
 			if (reset) rstBall();
 			reset = true;
 			speedY *= -1 + vTerminal;  // resistance percentage
-			ballPosY += speedY;
 		}
+		ballPosY += speedY;
 	}
 	
 	glutTimerFunc(30, ballBounce, 0); 
